29213218e99322d32d96470492e9e97a7029a398
KelseyCortez <kelscortez@gmail.com>
2020-04-24 15:25:59 -0400

    wip

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
-// const suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
-// const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+function createCard(point, suit) {
+    return { point, suit };
+}
+function createSuit(type) {
+    let suit = [];
+    // for (i = 1; i <= 13; i++) {
+    //     suit.push(createCard(i, type));
 
-// function createDeck() {
-//     deck = 
-// }
+    // }
+    values.forEach(function (item){
+        suit.push(createCard(item, type))
+    })
+
+
+    return suit;
+}
+
+function createDeck() {
+    let deck = [];
+    let hearts = createSuit('Hearts');
+    let spades = createSuit('Spades');
+    let diamonds = createSuit('Diamonds');
+    let clubs = createSuit('Clubs');
+
+
+    return deck.concat(hearts, spades, diamonds, clubs);
+}
+console.log(createDeck());
 
 
 const dealerHand = document.querySelector('#dealer-hand');
@@ -15,25 +38,25 @@ const hitButton = document.querySelector('#hit-button');
 const standButton = document.querySelector('#stand-button');
 
 
-dealButton.addEventListener('click', function(e){
+dealButton.addEventListener('click', function (e) {
     const card1 = document.createElement('img');
     const card2 = document.createElement('img');
     const card3 = document.createElement('img');
     const card4 = document.createElement('img');
-    card1.setAttribute('src', './images.5_of_hearts.png');
-    card2.setAttribute('src', './images.king_of_spades.png');
-    card3.setAttribute('src', './images.jack_of_diamonds.png');
-    card4.setAttribute('src', './images.2_of_clubs.png');
+    card1.setAttribute('src', './images/5_of_hearts.png');
+    card2.setAttribute('src', './images/king_of_spades.png');
+    card3.setAttribute('src', './images/jack_of_diamonds.png');
+    card4.setAttribute('src', './images/2_of_clubs.png');
     dealerHand.append(card1);
     dealerHand.append(card2);
     playerHand.append(card3);
-    playerHand.append(card4 );
+    playerHand.append(card4);
 
 })
 
-hitButton.addEventListener('click', function(e){
+hitButton.addEventListener('click', function (e) {
     const card = document.createElement('img');
-    card.setAttribute('src', './images.6_of_clubs.png');
+    card.setAttribute('src', './images/6_of_clubs.png');
     playerHand.append(card);
 
 })
